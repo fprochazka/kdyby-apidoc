ae991bc478ccf8efc8b0c3e4cf4c7b3be2f984c1
Filip Procházka <filip@prochazka.su>
2014-06-05 21:29:46 +0200

    init

---
+++ generate.php
@@ -0,0 +1,55 @@
+<?php
+
+// update deps
+use Nette\Utils\Finder;
+
+chdir(__DIR__);
+//passthru('composer update');
+
+// why not use then if we have them!
+require_once __DIR__ . '/vendor/autoload.php';
+
+$params = [
+	'rootDir' => __DIR__,
+	'vendorDir' => __DIR__ . '/vendor',
+	'wwwDir' => __DIR__ . '/www',
+];
+
+$settings = \Nette\Neon\Neon::decode(file_get_contents(__DIR__ . '/apigen.template.neon'));
+$settings = \Nette\DI\Helpers::expand($settings, $params);
+
+$settings['exclude'][] = $params['vendorDir'] . '/bin';
+$settings['exclude'][] = $params['vendorDir'] . '/composer';
+
+/** @var \SplFileInfo $packageDir */
+/** @var \SplFileInfo $excludeDir */
+foreach (Finder::findDirectories('*')->in($params['vendorDir']) as $vendorDir) {
+	foreach (Finder::findDirectories('*')->in($vendorDir) as $packageDir) {
+
+		// exclude in package
+		foreach (Finder::findDirectories('example*', 'style*', 'test*', 'doc*', 'tool*', '.git', 'bin')->in($packageDir) as $excludeDir) {
+			$settings['exclude'][] = $excludeDir->getPathname();
+		}
+
+		// doctrine
+		foreach (Finder::findDirectories('vendor')->limitDepth(1)->from($packageDir) as $excludeDir) {
+			$settings['exclude'][] = $excludeDir->getPathname();
+		}
+	}
+}
+
+// symfony
+foreach (Finder::findDirectories('Test*')->limitDepth(4)->from($params['vendorDir'] . '/symfony') as $excludeDir) {
+	$settings['exclude'][] = $excludeDir->getPathname();
+}
+
+// psr
+foreach (Finder::findDirectories('Test*')->limitDepth(3)->from($params['vendorDir'] . '/psr') as $excludeDir) {
+	$settings['exclude'][] = $excludeDir->getPathname();
+}
+
+file_put_contents(__DIR__ . '/apigen.neon', \Nette\Neon\Neon::encode($settings, \Nette\Neon\Neon::BLOCK));
+
+
+//passthru('/usr/local/bin/php -dmemory_limit=512M  /usr/local/bin/apigen --config "${API_DOC}/apigen.neon"');
+
